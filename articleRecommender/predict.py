99d3788bf5c26fdd9cd313080ce73b5549497993
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-05-18 10:15:46 +0300

    removed unnecessary file that is predictor.py

--- articleRecommender/predict.py
+++
@@ -1,8 +0,0 @@
-from .preProcessorModel import PopularityRecommender
-
-class PredictorClass:
-    def __init__(self) -> None:    
-        popular=PopularityRecommender()
-        popular.printer()
-    
-
